c03f88dc06a7a4c2763d014d923f05979d74973b
hemasrimarisetti <21b01a0598@svecw.edu.in>
2024-06-15 21:56:20 +0530

    4-prepared our app for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 
@@ -21,6 +22,7 @@ cloudinary.config({
 
 const app=express();
 const PORT=process.env.PORT || 5000;
+const __dirname = path.resolve()
 
 
 app.use(express.json({limit:"5mb"}));//to parse request.body
@@ -35,7 +37,12 @@ app.use("/api/users",userRoutes);
 app.use("/api/posts",postRoutes);
 app.use("/api/notifications",notificationRoutes);
 
-
+if(process.env.NODE_ENV === "production"){
+	app.use(express.static(path.join(__dirname,"/frontend/dist")));
+	app.get("*",(req,res)=>{
+		res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+	})
+}
 
 
 
